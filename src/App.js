b1dc8928942530029a1e64db0e317d4a9a97ff5c
Sodiq Ganiyu <gsolaniyi@gmail.com>
2024-01-11 21:18:54 +0100

    setup challenge 2 and make it work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <Intro />
+      <SkillList />
     </div>
   );
+}
+
+function Avatar() {
+  return <img src="claudio.jpg" alt="avatar" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Sodiq Ganiyu</h1>
+      <p>
+        I am a fantastic Frontend Developer with amazing UIUX skills. I make
+        $2,000 every month starting grom December 2025. I am a fantastic
+        Frontend Developer with amazing UIUX skills. I make $2,000 every month
+        starting grom December 2025.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return <div></div>;
+}
+
+function Skill() {
+  return <span></span>;
 }
 
 export default App;
